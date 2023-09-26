c9b1e78bbf98f80bc2fe37dcac792ffe498d5c10
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-09-26 08:25:34 +0000

    docs: output part done

--- app.js
+++ app.js
@@ -4,9 +4,35 @@ var cashGiven = document.querySelector("#cash-given");
 
 var checkButton = document.querySelector("#check-button");
 
-checkButton.addEventListener("click",()=> console.log("clicked"))
+var message = document.querySelector("#error-message")
 
+checkButton.addEventListener("click", function validateBillAndCashAmount () {
+    hideMessage();
 
+    if (billAmount.value>0){
+        if(cashGiven.value > billAmount.value){
+            var amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else { 
+            showMessage(
+                "Do yo wanna wash plates??"
+            );
+        }
+        
+    }else {  
+        showMessage ("invalid bill amount");
+        }
+    }
+);
 
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg)
+{
+    message.style.display = "block";
+    message.innerText = msg;
+}
 
  
